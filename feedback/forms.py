4d9258829ce5e079cea707893e2e4da4328b51e3
mubarak-mustopha <mustophamubarack17@gmail.com>
2025-11-09 12:41:13 +0100

    Fix typo and dict syntax error

--- feedback/forms.py
+++ feedback/forms.py
@@ -7,7 +7,7 @@ from django.core.mail import send_mail
 class FeedbackForm(forms.Form):
     email = forms.EmailField(label="Email Address")
     message = forms.CharField(
-            label="Message", widget=forms.Textarea(attr={"rows"=5})
+            label="Message", widget=forms.Textarea(attrs={"rows":5})
     )
 
     def send_email(self):
