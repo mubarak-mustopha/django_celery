f92034d4d94df40dcd3330666b9de10b9aabae3c
mubarak-mustopha <mustophamubarack17@gmail.com>
2025-11-09 12:30:52 +0100

    Set up views, urls and templates to get user feedback.

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
+from feedback.forms import FeedbackForm
 
-# Create your views here.
+
+class FeedbackFormView(FormView):
+    template_name = "feedback/feedback.html"
+    form_class = FeedbackForm
+    success_url = "/success/"
+
+class SuccessView(TemplateView):
+    template_name = "feedback/success.html"
